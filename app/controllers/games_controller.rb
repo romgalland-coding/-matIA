1af102d099dbf0f1226ccc4f14aee602ac070106
carlfarra <carlfarra@gmail.com>
2026-05-26 12:57:16 +0200

    added ability to destroy a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,11 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = current_user.games
+  end
+
+  def destroy
+    @game = current_user.games.find(params[:id])
+    @game.destroy
+    redirect_to games_path, notice: "#{game.title} removed from wishlist."
   end
 end
