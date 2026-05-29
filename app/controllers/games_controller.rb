f7e4a55a565ba367edb1c4c2ccae78a3ecc9292c
Maxime Baboin <maxime@baboin.com>
2026-05-29 11:34:15 +0200

    updated sort-by and added filter by options

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,20 @@ class GamesController < ApplicationController
   def index
     @sort = params[:sort].presence_in(Game::SORT_FIELDS) || "title"
     @direction = params[:direction].presence_in(Game::SORT_DIRECTIONS) || "asc"
-    @games = current_user.games.sorted_by(@sort, @direction)
+    @selected_platform = params[:platform]
+    @selected_genre = params[:genre]
+    @selected_studio = params[:studio]
+
+    library_games = current_user.games.where(collection_status: %w[wishlist played])
+    library_platforms = library_games.pluck(:platform).flat_map { |platform| Array(platform) }.compact.uniq
+
+    @platform_options = Array(current_user.devices).select { |device| library_platforms.include?(device) }.uniq
+    @genre_options = library_games.distinct.pluck(:genre).compact.sort
+    @studio_options = library_games.distinct.pluck(:studio).compact.sort
+
+    @games = library_games
+             .filter_by(@selected_platform, @selected_genre, @selected_studio)
+             .sorted_by(@sort, @direction)
   end
 
   def update
